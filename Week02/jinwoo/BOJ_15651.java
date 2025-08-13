7932fa0fb926487c9c95c28f5e0cdf971eba9b66
Jin_____w <130946519+K-JW@users.noreply.github.com>
2025-08-13 14:58:11 +0900

    Create BOJ_15651.java

---
+++ Week02/jinwoo/BOJ_15651.java
@@ -0,0 +1,59 @@
+import java.util.*;
+import java.io.*;
+
+public class BOJ_15651 {
+	
+	static int[] perm;
+	static boolean[]visited;
+	static int N;
+	static int M;
+	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		
+		st = new StringTokenizer(br.readLine());
+		
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		
+		perm = new int[M];
+		visited = new boolean[N+1];
+		
+		permutation(0);
+		
+		bw.flush();
+		bw.close();
+		br.close();
+		
+	}
+	
+	static void permutation(int depth) throws IOException {
+
+		if (depth == M) {
+			
+			for (int i = 0; i < M; i++) {
+				
+				bw.write(perm[i] + " ");
+				
+			}
+			
+			bw.newLine();
+			
+			return;
+		}
+		
+		for (int i = 1; i <= N; i++) {
+				
+				perm[depth] = i;
+				
+				permutation(depth+1);
+				
+				
+			}
+		}
+		
+}
