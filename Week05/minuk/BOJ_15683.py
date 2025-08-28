b23e6cf32ed5e6b46f6f45fe0a855aaab6ff58df
KIMMINUK <kmwpcpk@gmail.com>
2025-08-28 23:38:40 +0900

    0828

--- Week05/minuk/BOJ_15683.py
+++ Week05/minuk/BOJ_15683.py
@@ -21,27 +21,66 @@ ex)
 import sys
 input = sys.stdin.readline
 
-dx = [1, -1, 0, 0]
-dy = [0, 0, 1, -1]
+# 상 하 우 좌
+dx = [0, 0, 1, -1]
+dy = [1, -1, 0, 0]
 
+def move(x, y, graph, dir):
+    nx = x + dx[dir]
+    ny = y + dy[dir]
+    while 0 <= nx < m and 0 <= ny < n and graph[ny][nx] != 6:
+        if graph[ny][nx] == 0:
+            graph[ny][nx] = -1
+        nx += dx[dir]
+        ny += dy[dir]
+            
 def dfs(idx):
+    global result
     if idx == len(cctv_list):
+        copy_graph = [row[:] for row in graph] # 임시 그래프 (원본 안건드리기)
         # temp에는 t, (x, y), 방향
         for t, coor, d in temp:
             x, y = coor
-            if t == 1:
-                nx = x + dx[d]
-                ny = y + dy[d]
-                
-                return
-            elif t == 2:
-                return
-            elif t == 3:
-                return
-            elif t == 4:
-                return
+            if t == 1: # 0 1 2 3
+                move(x, y, copy_graph, d)
+            elif t == 2: # 0 2 // 01 23
+                    move(x, y, copy_graph, d)
+                    move(x, y, copy_graph, d+1)
+            elif t == 3: # 0 1 2 3 // 02, 21, 13, 30
+                if d == 0 or d == 1:
+                    move(x, y, copy_graph, d)
+                    move(x, y, copy_graph, d+2)
+                elif d == 2:
+                    move(x, y, copy_graph, d)
+                    move(x, y, copy_graph, d-1)
+                else:
+                    move(x, y, copy_graph, d)
+                    move(x, y, copy_graph, d-3)
+            elif t == 4: # 0 1 2 3
+                if d == 0 or d == 1:
+                    move(x, y, copy_graph, d)
+                    move(x, y, copy_graph, d+1)
+                    move(x, y, copy_graph, d+2)
+                elif d == 2:
+                    move(x, y, copy_graph, d)
+                    move(x, y, copy_graph, d+1)
+                    move(x, y, copy_graph, d-2)
+                else:
+                    move(x, y, copy_graph, d)
+                    move(x, y, copy_graph, d-3)
+                    move(x, y, copy_graph, d-2)
             elif t == 5:
-                return
+                move(x, y, copy_graph, d)
+                move(x, y, copy_graph, d+1)
+                move(x, y, copy_graph, d+2)
+                move(x, y, copy_graph, d+3) 
+                
+        cnt = 0
+        for y in range(n):
+            for x in range(m):
+                if copy_graph[y][x] == 0:
+                    cnt += 1
+        result = min(result, cnt)
         return
     
     t, x, y = cctv_list[idx]
@@ -52,7 +91,7 @@ def dfs(idx):
             dfs(idx+1)
             temp.pop()
     elif t == 2:
-        for d in range(2):
+        for d in range(0, 3, 2):
             temp.append((t, (x, y), d))
             dfs(idx+1)
             temp.pop()
@@ -74,6 +113,7 @@ def dfs(idx):
 n, m = map(int, input().split())
 graph = [list(map(int, input().split())) for _ in range(n)]
 temp = []
+result = 1e9
 
 cctv_list = []
 for y in range(n):
@@ -82,3 +122,5 @@ for y in range(n):
             cctv_list.append((graph[y][x], x, y))
 
 dfs(0)
+
+print(result)
